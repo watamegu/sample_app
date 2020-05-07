95840c66d71156ab4f2ff377011dd0e89f3af8b0
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-07 18:20:35 +0900

    aboutページとテストの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home' # => StaticPages#home
   get 'static_pages/help' # => StaticPages#help
+  get 'static_pages/about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
