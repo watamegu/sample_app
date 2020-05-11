feefc8f638d952b29a8204495f55d07684e2f1a4
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-11 18:51:12 +0900

    ユーザー登録失敗時の挙動を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  #GET /users/new
   def new
+    @user = User.new
   end
+
+  #POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #Success
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
