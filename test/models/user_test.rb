a3a45e076bdfbeee9e5d7f58b8e646667eaa2e4b
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-11 15:35:33 +0900

    Emailの保存前に小文字化する処理を追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,8 +50,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be unique" do
-    duplicate_user       = @user.dup
-    duplicate_user.email = @user.email.upcase
+    duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
   end
