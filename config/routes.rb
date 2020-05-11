1e0e23a7f764bdff9f707d280e274a3aa2e9b699
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-11 11:01:56 +0900

    Contactページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home' # => StaticPages#home
   get 'static_pages/help' # => StaticPages#help
-  get 'static_pages/about' 
+  get 'static_pages/about'  # => StaticPages#About
+  get 'static_pages/contact' # => StaticPages#Contact
 end
