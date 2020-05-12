29524a3fdafd61d381ff2b5938943d1d97df9dcb
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-12 10:44:20 +0900

    ユーザー登録機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # => redirect_to user_path(@user)
+      #GET "users/#{@user.id}"
     else
       render 'new'
     end
