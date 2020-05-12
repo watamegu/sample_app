23426663ab138a21948d848640353c0e418ccdd9
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-12 15:24:43 +0900

    ログイン機能を実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  #GET /login
+  def new
+    #scope: :session + url: login_path
+  end
+
+  #POST /login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,21 @@
+module SessionsHelper
+
+	# 渡されたユーザーでログインする
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+# 現在ログイン中のユーザーを返す (いる場合)
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+			#@current_userがnilの場合のみ、User.find~を実行。2回目以降はDBへ検索を行わず、処理を軽くする。
+		end
+	end
+
+# ユーザーがログインしていれば true、その他なら false を返す
+	def logged_in?
+		!current_user.nil? #current_userがnilでないことを確認
+	end
+
+end
