90cc44232249544eb405b7411460ef086e4be0df
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-14 18:25:30 +0900

    パスワードの再設定機能を実装

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+
+  # アカウント有効化メールの送信
   test "account_activation" do
     user = users(:michael)
     user.activation_token = User.new_token
@@ -12,5 +14,18 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.name,                mail.body.encoded
     assert_match user.activation_token,    mail.body.encoded
     assert_match CGI.escape(user.email),   mail.body.encoded
+  end
+
+  #パスワードリセットメールの送信
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 end
