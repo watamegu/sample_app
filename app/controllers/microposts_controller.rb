d5076c518acdc339bded03f526999697a40bc1dc
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-15 20:06:16 +0900

    debugger消去

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,6 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     @micropost.image.attach(params[:micropost][:image])
-    debugger
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
