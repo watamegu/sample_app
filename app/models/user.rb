1e56b10141d3f982fabcee96e02007d052b064f8
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-18 16:08:35 +0900

    Relationshipモデルのバリデーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship",
+																	foreign_key: "follower_id",
+																	dependent: :destroy
+	has_many :following, 						through: :active_relationships,
+																	source: :followed
 
 	attr_accessor :remember_token,
 								:activation_token,
@@ -88,6 +93,22 @@ class User < ApplicationRecord
 	# 完全な実装は次章の「ユーザーをフォローする」を参照
 	def feed
 		Micropost.where("user_id = ?", self.id)
+	end
+
+	#フォロー関連
+	# ユーザーをフォローする
+	def follow(other_user)
+		following << other_user
+	end
+
+	# ユーザーをフォロー解除する
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	# 現在のユーザーがフォローしてたら true を返す
+	def following?(other_user)
+		following.include?(other_user)
 	end
 
 	private
