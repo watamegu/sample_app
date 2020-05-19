b7b408627cdd707bf58c7d30c0950c8d329dbdd3
Megumi Watanabe <watanabe_megumi@lightcafe.co.jp>
2020-05-19 11:55:28 +0900

    TLに自分とフォローしているユーザーの投稿を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,8 +94,12 @@ class User < ApplicationRecord
 	end
 
 	# -- フィード関連 --
+	# ユーザーのステータスフィードを返す
 	def feed
-		Micropost.where("user_id = ?", self.id)
+		following_ids = "SELECT followed_id FROM relationships
+											WHERE follower_id = :user_id"
+		Micropost.where("user_id IN (#{following_ids})
+											OR user_id = :user_id", user_id: id)
 	end
 
 	# -- フォロー関連 --
